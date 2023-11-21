e034aae0f7c09b88c2a061989b6ef084b01564da
rphii <raphael.seitz18@gmail.com>
2023-11-22 00:00:02 +0100

    more thoroughly test str_fmt

--- test/src/main.c
+++ test/src/main.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include "unity.h"
 #include "unity_internals.h"
 #include "str.h"
@@ -40,6 +42,29 @@ void test_int_123(void) {
     TEST_ASSERT_EQUAL('2', str_get_at(&string, 1));
     TEST_ASSERT_EQUAL('3', str_get_at(&string, 2));
     TEST_ASSERT_EQUAL(0, string.s[3]); /* brute-force access, because otherwise out-of-bounds */
+
+    str_free(&string);
+}
+
+void test_many_ints(void) {
+    Str string = {0};
+    size_t n = 100000;
+    for(size_t i = 0; i < n; i++) {
+        TEST_ASSERT_EQUAL(0, str_fmt(&string, "%u", i));
+    }
+    size_t expected_len = 0;
+    for(size_t i = 0; i < n; i++) {
+        size_t digits = i > 0 ? (size_t)(log10((double)i)+1) : 1;
+        for(size_t j = 0; j < digits; j++) {
+            size_t pow10 = (size_t)pow(10, (double)(digits-j-1));
+            char digit = (i / pow10) % 10 + '0';
+            TEST_ASSERT_EQUAL(digit, str_get_at(&string, expected_len+j));
+        }
+        expected_len += digits;
+    }
+    TEST_ASSERT_EQUAL(expected_len, str_length(&string));
+
+    str_free(&string);
 }
 
 // not needed when using generate_test_runner.rb
@@ -47,6 +72,7 @@ int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_basics);
     RUN_TEST(test_int_123);
+    RUN_TEST(test_many_ints);
     printf("\ndone\n");
     return UNITY_END();
 }
