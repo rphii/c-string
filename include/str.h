f931acd81906a80527bcfc66d5439f4a8494320f
rphii <raphael.seitz18@gmail.com>
2023-11-21 23:27:36 +0100

    set default size

--- include/str.h
+++ include/str.h
@@ -1,7 +1,10 @@
 #ifndef STR_H
 
+#define STR_DEFAULT_SIZE 32
+
 /* configuration, inclusion and de-configuration of vector */
 
+#define VEC_SETTINGS_DEFAULT_SIZE STR_DEFAULT_SIZE
 #define VEC_SETTINGS_KEEP_ZERO_END 1
 #define VEC_SETTINGS_STRUCT_ITEMS s
 #include "vec.h"
@@ -10,6 +13,7 @@ VEC_INCLUDE(Str, str, char, BY_VAL);
 
 #undef VEC_SETTINGS_STRUCT_ITEMS
 #undef VEC_SETTINGS_KEEP_ZERO_END
+#undef VEC_SETTINGS_DEFAULT_SIZE
 
 /* other functions */
 
